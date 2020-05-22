85cd3b1b71492ecf20537bec1f0c114b334dcbd6
Mehtaab Gill <37054853+mehtaabGill@users.noreply.github.com>
2020-05-21 19:12:44 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const fileUpload = require('express-fileupload');
+const app = express();
+app.disable('x-powered-by');
+
+//SETTINGS
+const MB = 1000000;
+const MAX_FILE_SIZE = MB * 500; //500mb
+const ACCEPTED_FILE_MIME_TYPE = 'text/javascript';
+const PORT = 3000;
+
+app.use(fileUpload({
+    limits: { fileSize: MAX_FILE_SIZE },
+    abortOnLimit: true,
+    limitHandler: (req, res, next) => {
+        res.status(413).send('File too large for upload');
+        next(res);
+        return;
+    },
+    useTempFiles: true
+}));
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html');
+})
+
+app.post('/upload', function(req, res) {
+  if (!req.files || Object.keys(req.files).length === 0) {
+    return res.status(400).send('No files were uploaded.');
+  }
+
+  let file = req.files.file;
+
+  if(!file || file.mimetype != ACCEPTED_FILE_MIME_TYPE) {
+    return res.status(400).send('Invalid file type. Javascript files only.');
+  }
+
+  //DO STUFF WITH FILE
+  //SEND RESPONSE WITH DEOBF FILE
+  res.redirect('/');
+
+});
+
+app.use((req, res, next) => {
+    res.redirect('/');
+})
+
+app.listen(PORT, () => {
+    console.log(`http://localhost:${PORT}`)
+});
